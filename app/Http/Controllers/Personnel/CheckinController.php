52a0732c104be27796b6d97b64cf0ff4668ad31c
Bryan Mulingbayan <bmulingbayan@yondu.com>
2022-01-29 20:21:38 +0800

    WIP: Checkins

---
+++ app/Http/Controllers/Personnel/CheckinController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Personnel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckinController extends Controller
+{
+    public function store()
+    {
+        # code...
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Personnel\AuthController;
+use App\Http\Controllers\Personnel\AuthController as PersonnelAuthController;
+use App\Http\Controllers\Personnel\CheckinController as PersonnelCheckinController;
 use App\Http\Controllers\Personnel\PersonnelMpinController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,11 +23,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'personnel'], function () {
-        Route::post('login', [AuthController::class, 'login']);
+        Route::post('login', [PersonnelAuthController::class, 'login']);
 
         Route::group(['middleware' => 'auth:personnels'], function () {
-            Route::get('/details', [AuthController::class, 'details']);
+            Route::get('/details', [PersonnelAuthController::class, 'details']);
             Route::post('/mpin', [PersonnelMpinController::class, 'store']);
+
+            Route::get('/checkins', [PersonnelCheckinController::class, 'index']);
         });
     });
 });
