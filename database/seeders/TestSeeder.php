f9f8f6fae9e2476a1ac9d834e985e4c04f0cc936
Bryan Mulingbayan <bmulingbayan@yondu.com>
2022-02-06 13:07:24 +0800

    Add test seeder

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Checkin;
+use App\Models\Jurisdiction;
+use App\Models\Personnel;
+use App\Models\Station;
+use App\Models\SubUnit;
+use App\Models\Unit;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Unit::factory()->count(5)
+            ->create()
+            ->each(function ($unit) {
+                User::factory()
+                    ->regionalPoliceOfficer()
+                    ->count(3)
+                    ->create(['unit_id' => $unit->id]);
+
+                // Create Sub Units.
+                SubUnit::factory()
+                    ->count(5)
+                    ->create(['unit_id' => $unit->id])
+                    ->each(function ($subUnit) use ($unit) {
+                        User::factory()
+                            ->provincialPoliceOfficer()
+                            ->count(5)
+                            ->create([
+                                'unit_id' => $unit->id,
+                                'sub_unit_id' => $subUnit->id
+                            ]);
+
+                        // Create Stations
+                        Station::factory()
+                            ->count(5)
+                            ->create(['sub_unit_id' => $subUnit->id])
+                            ->each(function ($station) use ($unit, $subUnit) {
+                                User::factory()
+                                    ->provincialPoliceOfficer()
+                                    ->count(5)
+                                    ->create([
+                                        'unit_id' => $unit->id,
+                                        'sub_unit_id' => $subUnit->id,
+                                        'station_id' => $station->id
+                                    ]);
+                                
+                                Jurisdiction::factory()
+                                    ->count(5)
+                                    ->create(['station_id' => $station->id])
+                                    ->each(function ($jurisdiction)  {
+                                        Personnel::factory()->count(5)
+                                            ->create(['jurisdiction_id' => $jurisdiction->id])
+                                            ->each(function ($personnel) {
+                                                Checkin::factory()
+                                                    ->count(3)
+                                                    ->create(['personnel_id', $personnel->id]);
+
+                                                Checkin::factory()
+                                                    ->absent()
+                                                    ->count(2)
+                                                    ->create(['personnel_id', $personnel->id]);
+                                            });
+                                    });
+                            });
+                    });
+            });
+    }
+}
