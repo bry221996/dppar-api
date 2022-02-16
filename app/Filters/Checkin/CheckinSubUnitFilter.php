d04d7c40b69c32162ed43941b35e9cc4951e58e9
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2022-02-16 23:21:49 +0800

    checkin

---
+++ app/Filters/Checkin/CheckinSubUnitFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filters\Checkin;
+
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\Filters\Filter;
+
+class CheckinSubUnitFilter implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property)
+    {
+        return $query->when($value, function ($subQuery) use ($value) {
+            $subQuery->whereHas('personnel', function ($personnelQuery) use ($value) {
+                $personnelQuery->whereHas('assignments', function ($assignmentQuery) use ($value) {
+                    $assignmentQuery->where('sub_unit_id', $value);
+                });
+            });
+        });
+    }
+}
--- app/Http/Controllers/Admin/CheckinController.php
+++ app/Http/Controllers/Admin/CheckinController.php
@@ -2,23 +2,31 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Filters\CheckinFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ListPersonnelRequest;
-use App\Repositories\CheckinRepository;
+
+use App\Models\Checkin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+use App\Filters\Checkin\CheckinUnitFilter;
+use App\Filters\Checkin\CheckinSubUnitFilter;
+use App\Filters\Checkin\CheckinStationFilter;
 
 class CheckinController extends Controller
 {
-    protected $checkinRepository;
-
-    public function __construct(CheckinRepository $checkinRepository)
+    public function index(Request $request)
     {
-        $this->checkinRepository = $checkinRepository;
-    }
+        $user = Auth::guard('admins')->user();
 
-    public function index(ListPersonnelRequest $request, CheckinFilter $checkinFilter)
-    {
-        $list = $this->checkinRepository->listWithFilters($checkinFilter, $request->per_page ?? 10);
+        $list = QueryBuilder::for(Checkin::class)
+            ->allowedFilters([
+                AllowedFilter::custom('unit_id', new CheckinUnitFilter)->default($user->unit_id),
+                AllowedFilter::custom('sub_unit_id', new CheckinSubUnitFilter)->default($user->sub_unit_id),
+                AllowedFilter::custom('station_id', new CheckinStationFilter)->default($user->station_id),
+            ])
+            ->paginate($request->per_page ?? 10);
 
         return response($list);
     }
